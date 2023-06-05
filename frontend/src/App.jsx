ad03fa6ec798f133c1e95d3270c479f3ec8c229e
asimdelvi <asimdelvi30@gmail.com>
2023-06-05 09:34:48 +0530

    add link to header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,9 +15,12 @@ function App() {
   return (
     <>
       <nav className="transition duration-700 ease-in px-20 flex items-center justify-between nav-background sticky top-0 py-4 border-0 rounded-none">
-        <h1 className="text-lg font-mono font-extrabold">eLibrary</h1>
+        <NavLink to="/">
+          <h1 className="text-lg font-mono font-extrabold">eLibrary</h1>
+        </NavLink>
+
         <div className="flex items-center">
-          <NavLink className="px-4 group" to="/">
+          <NavLink className="mr-4 group" to="/">
             <div className="rounded-full flex">
               <BiHomeAlt2 className="rounded-full bg-black p-[6px] box-content" />
               <span className="animate-border pr-3 pl-5 py-[3px]  animate-nav">
@@ -25,7 +28,7 @@ function App() {
               </span>
             </div>
           </NavLink>
-          <NavLink className="px-4 group" to="/books">
+          <NavLink className="mr-4 group" to="/books/all">
             <div className="rounded-full flex">
               <TbBooks className="rounded-full bg-black p-[6px] box-content" />
               <span className="animate-border pr-3 pl-5 py-[3px]  animate-nav">
@@ -33,7 +36,7 @@ function App() {
               </span>
             </div>
           </NavLink>
-          <NavLink className="mx-4 group" to="/books/new">
+          <NavLink className="mr-4 group" to="/books/new">
             <div className="rounded-full flex">
               <BiBookAdd className="rounded-full bg-black p-[6px] box-content" />
               <span className="animate-border pr-3 pl-5 py-[3px]  animate-nav">
@@ -44,12 +47,12 @@ function App() {
         </div>
         <div>
           <NavLink to="/Login">
-            <span className="rounded-xl bg-black border-black border-2 text-white px-3 py-[6px] text-sm">
+            <span className="rounded-xl bg-black border-black border-2 text-white px-3 py-[6px] text-sm hover:bg-gray-800">
               Login
             </span>
           </NavLink>
           <NavLink to="/Register">
-            <span className="ml-4 rounded-xl border-black border-2 px-3 py-[6px] text-sm">
+            <span className="ml-4 rounded-xl border-black border-2 px-3 py-[6px] text-sm hover:border-gray-600 hover:text-gray-800">
               Register
             </span>
           </NavLink>
@@ -57,7 +60,7 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/books" element={<Books />} />
+        <Route path="/books/all" element={<Books />} />
         <Route path="/books/new" element={<NewBook />} />
         <Route path="/books/:id" element={<Book />} />
         <Route path="/books/:id/update" element={<UpdateBook />} />
