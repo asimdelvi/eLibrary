c4b40e6aa8285102cebd7b21cd18d08b64d8bd3c
asimdelvi <asimdelvi30@gmail.com>
2023-06-20 19:39:42 +0530

    design forms

--- frontend/src/pages/books/Books.jsx
+++ frontend/src/pages/books/Books.jsx
@@ -1,26 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBooks } from "../../redux/features/bookSlice";
 import { Link } from "react-router-dom";
+import { BASE_URL } from "../../redux/api/books";
 
 export const Books = () => {
   const dispatch = useDispatch();
   const { books } = useSelector((state) => state.books);
+  // const [isActive, setIsActive] = useState(false);
+  const [clickedIndex, setClickedIndex] = useState();
 
   useEffect(() => {
     dispatch(getBooks());
   }, [dispatch]);
 
+  const handleClick = (index) => {
+    setClickedIndex(index);
+    // setIsActive((prev) => {
+    //   return !prev;
+    // });
+  };
+
   return (
     <>
-      <div className="px-24 grid grid-cols-3 gap-12 my-10">
+      <div className="px-24 grid grid-cols-3 grid-flow-dense gap-12 my-10">
         {books.map((book, index) => (
           <div
             key={index}
-            className="flex items-center justify-center min-h-[10rem] border-2 border-gray-700 shadow-lg bg-[#dfd2b9] rounded-3xl"
+            className={
+              clickedIndex === index
+                ? "transition ease-linear duration-700 flex flex-col items-center justify-center min-h-[10rem]  border-gray-700 border-[1px] shadow-xl rounded-xl col-span-2 bg-[#dad9d9]"
+                : "transition ease-linear duration-700 flex flex-col items-center justify-center min-h-[10rem]  border-gray-700 border-[1px] shadow-xl rounded-xl bg-[#dad9d9]"
+            }
+            onClick={() => handleClick(index)}
           >
-            <h1 className="text-lg"> {book.title} </h1>
-            {/* <Link to={`/books/${book._id}`}>View</Link> */}
+            <h1 className="text-lg px-7 py-2 text-clip"> {book.title} </h1>
+
+            {clickedIndex === index ? (
+              <div>
+                <Link to={`/books/${book._id}`}>
+                  <span className="mr-1 rounded-lg bg-[#B59D9A] border-[#B59D9A] border-2  px-3 py-[6px] text-sm hover:shadow-md">
+                    View
+                  </span>
+                </Link>
+                <Link to={`${BASE_URL}/books/download/${book.pdfURL}`} download>
+                  <span className="rounded-lg bg-black border-black border-2 text-white px-3 py-[6px] text-sm hover:shadow-md">
+                    Download
+                  </span>
+                </Link>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         ))}
       </div>
