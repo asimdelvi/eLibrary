ce9eee00de68f78ae89f3eb3e3c04cec39e1409f
asimdelvi <asimdelvi30@gmail.com>
2023-06-21 18:57:58 +0530

    change production caps

--- frontend/src/redux/api/books.js
+++ frontend/src/redux/api/books.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const BASE_URL =
-  process.env.NODE_ENV !== "Production"
+  process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_BASE_URL_LOCAL
     : process.env.REACT_APP_BASE_URL_PROD;
 
